21158094940e3b40b20d395bc83cd8d309fbcf29
Jan Lindblom <lindblom.jan@gmail.com>
2011-11-24 20:02:29 +0100

    Adding NetBeans project.

---
+++ src/se/janlindblom/libepub/Document.java
@@ -0,0 +1,11 @@
+package se.janlindblom.libepub;
+
+/**
+ * Represents an EPUB document.
+ *
+ * @author Jan Lindblom <lindblom.jan@gmail.com>
+ * @version 0.0.1
+ */
+public class Document {
+    
+}
